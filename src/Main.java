b44ae748e28bf870e66c9776b8d4c5d8580301f9
AlexanderPaul1983 <alexanderpaul@gmx.de>
2023-06-29 17:10:32 +0200

    Strategie Pattern

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,20 @@
-import EasyTasks.*;
+import EasyTasks.AbstrakteStrategie;
+import EasyTasks.KonkreteStrategieA;
+import EasyTasks.KonkreteStrategieB;
+import EasyTasks.Nutzer;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        Rectangle rechteck = new Rectangle(5,6);
-        System.out.println(rechteck.calculateArea());
-        Circle kreis = new Circle(4);
-        System.out.println(kreis.calculateArea());
 
-        }
+        AbstrakteStrategie strategieA = new KonkreteStrategieA();
+        strategieA.operation();
+        AbstrakteStrategie  strategieB = new KonkreteStrategieB();
+        strategieB.operation();
+
+
+
     }
+
+
+}
+
