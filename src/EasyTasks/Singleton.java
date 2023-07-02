861767adfc1d5925b03e4379d87bf5c64fede28a
AlexanderPaul1983 <alexanderpaul@gmx.de>
2023-07-02 19:48:05 +0200

    Singleton pattern was added

---
+++ src/EasyTasks/Singleton.java
@@ -0,0 +1,25 @@
+package EasyTasks;
+
+public class Singleton {
+    public  String name;
+    private static Singleton king;
+
+    private Singleton(){};
+
+    public static Singleton getInstance(){
+        if(king == null){
+            king = new Singleton();
+
+        }
+        return king;
+    }
+    public void setName(String koenig){
+        name = koenig;
+    }
+    public void Befehl1(){
+        System.out.println(name + " Ich befehle");
+    }
+    public void Befehl2(){
+        System.out.println("Ich befehle nicht");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,15 @@
-import EasyTasks.AbstrakteStrategie;
-import EasyTasks.KonkreteStrategieA;
-import EasyTasks.KonkreteStrategieB;
-import EasyTasks.Nutzer;
+import EasyTasks.*;
 
 public class Main {
     public static void main(String[] args) {
-        // Hier werden die Objekte Strategie A und B erstellt
 
-        AbstrakteStrategie Strategy_A = new KonkreteStrategieA();
-        AbstrakteStrategie Strategy_B = new KonkreteStrategieB();
-
-// und hier werden diese Objekte an Nutzer übergeben, man könnte Nutzer vielleicht als Play nennen, aber ist Geschmack Sache
-        Nutzer player1 = new Nutzer(Strategy_A);
-        player1.operation();
-        Nutzer player2 = new Nutzer(Strategy_B);
-        player2.operation();
+        Singleton Ludwig = Singleton.getInstance();
+        Ludwig.setName("Ludwig");
+        Ludwig.Befehl1();
+        Singleton Heinrich = Singleton.getInstance();
+        Heinrich.setName("Heinrich");
+        Heinrich.Befehl1();
+        Ludwig.Befehl1();
 
 
 
